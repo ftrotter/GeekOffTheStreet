35cf5208a24b479e7cccb720a60147c1b3bc6e4b
Fred Trotter <fred.trotter@gmail.com>
2023-01-12 23:50:40 +0000

    generates sql

--- py/import_comment_data_to_mariadb.py
+++ py/import_comment_data_to_mariadb.py
@@ -5,9 +5,16 @@
 import sqlalchemy
 import yaml
 import pandas as pd
+import glob
+import os
+import json
+import pprint
+import re
+from bs4 import BeautifulSoup,  MarkupResemblesLocatorWarning
+import warnings
 
 with open('../database.yaml', 'r') as yaml_file:
-       database = yaml.safe_load(yaml_file)
+    database = yaml.safe_load(yaml_file)
 
 api_key = ['regulations_gov_api_key']
 
@@ -25,11 +32,54 @@ engine = sqlalchemy.create_engine(sql_url)
 #From here on down, we can do anything that Pandas and SQLAlchemy can do
 #With data from the MySQL database.
 
-#lets show a few databases from the metadata.. as a test!!
-select_sql = f"""
-SELECT * FROM geek.comment  
+#We will need two globs (at least) to import all the data.. one for json and one for text..
+
+
+warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
+
+#oftimes html sneaks throough into the comments.. we want all of it gone!!
+def parse_html(html):
+    soup = BeautifulSoup(html,features="html.parser")
+    return(soup.get_text())
+
+def simplify_comment_string(comment):
+    # do not want any puctuation or special characters. Just words, then make it all lowercase...  
+    comment = parse_html(comment)
+
+    comment = re.sub('[^a-zA-Z0-9 \n\.]', '', comment)
+    comment = comment.lower()
+    comment = ' '.join(comment.split()) #removes the double spaces in the text
+    return(comment)
+
+data_dir = './data/'
+db = 'geek'
+comment_table = 'comment'
+
+file_list = glob.glob(data_dir + '/*.json') # all of the raw comments json
+
+comment_source = 'inline_json'
+
+for file_name in file_list:
+    f = open(file_name)
+    jdata = json.load(f)
+
+    comment_id = jdata['data']['id']
+    
+    if 'comment' in jdata['data']['attributes']:
+        comment = jdata['data']['attributes']['comment']
+        if(comment is not None):
+            comment = simplify_comment_string(comment)
+
+            replace_sql = f"""
+REPLACE INTO {db}.{comment_table} 
+    (`id`, `comment_identifier`, 
+    `comment_source`, `comment_filepath`, `comment_text`) 
+VALUES 
+    (NULL, '{comment_id}', '{comment_source}', '{file_name}', '{comment}');
+
 """
-
-df = pd.read_sql_query(select_sql, engine)
-print(df)
-
+            print(replace_sql)
+        else:
+            print(f"comment is None in {file_name}")
+    else:
+        print(f"no comment in {file_name}")
